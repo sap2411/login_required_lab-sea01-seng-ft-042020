e0f99a441881e4519e701b2cd944f8ad0277d2f2
sap2411 <steven.parsons.us@gmail.com>
2020-05-26 08:36:36 -0700

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/show' => 'secrets#show'
+  get '/new' => 'sessions#new'
+  post '/' => 'sessions#create'
+  post '/' => 'sessions#destroy'
 end
